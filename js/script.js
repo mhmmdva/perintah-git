2594f036d52d7cbeb9260db6e7993ea32668040c
mhmmdva <vallenleyss@gmail.com>
2022-08-13 23:13:27 +0700

    modified css, index, js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,24 @@
-const btnContainer = document.getElementById('sectionA')
-const btns = document.getElementsByClassName('btn')
-
-for (let i = 0; i < btns.length; i++) {
-   btns[i].addEventListener('click', function () {
-      let current = document.getElementsByClassName('active')
-      current[0].className = current[0].className.replace('active')
-      this.className += 'active'
-   })
-}
-
-const linkAktif1 = document.querySelector('#sectionA .a-satu')
+// const linkAktif1 = document.querySelector('#sectionA .a-satu')
 const linkAktif2 = document.querySelector('#sectionA .a-dua')
 const linkAktif3 = document.querySelector('#sectionA .a-tiga')
 const linkAktif4 = document.querySelector('#sectionA .a-empat')
 const linkAktif5 = document.querySelector('#sectionA .a-lima')
 const linkAktif6 = document.querySelector('#sectionA .a-enam')
+const linkAktif7 = document.querySelector('#sectionA .a-tujuh')
+const linkAktif8 = document.querySelector('#sectionA .a-delapan')
+const linkAktif9 = document.querySelector('#sectionA .a-sembilan')
 
-const p1 = document.querySelector('#sectionA .p-satu .ul-satu')
+// const p1 = document.querySelector('#sectionA .p-satu .ul-satu')
 const p2 = document.querySelector('#sectionA .p-dua .ul-dua')
 const p3 = document.querySelector('#sectionA .p-tiga .ul-tiga')
 const p4 = document.querySelector('#sectionA .p-empat .ul-empat')
 const p5 = document.querySelector('#sectionA .p-lima .ul-lima')
 const p6 = document.querySelector('#sectionA .p-enam .ul-enam')
+const p7 = document.querySelector('#sectionA .p-tujuh .ul-tujuh')
+const p8 = document.querySelector('#sectionA .p-delapan .ul-delapan')
+const p9 = document.querySelector('#sectionA .p-sembilan .ul-sembilan')
 
-linkAktif1.addEventListener('click', function () {
-   p1.classList.toggle('ul-satu');
-});
+
 linkAktif2.addEventListener('click', function () {
    p2.classList.toggle('ul-dua');
 });
@@ -40,5 +33,14 @@ linkAktif5.addEventListener('click', function () {
 });
 linkAktif6.addEventListener('click', function () {
    p6.classList.toggle('ul-enam');
+});
+linkAktif7.addEventListener('click', function () {
+   p7.classList.toggle('ul-tujuh');
+});
+linkAktif8.addEventListener('click', function () {
+   p8.classList.toggle('ul-delapan');
+});
+linkAktif9.addEventListener('click', function () {
+   p9.classList.toggle('ul-sembilan');
 });
 
